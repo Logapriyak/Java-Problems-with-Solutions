3f6ee82550b250cbbd5bf2d14d66176ba516ed60
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 21:15:13 +0530

    Add switch expression with return example
    Demonstrates the use of an enhanced switch expression to return the name of the day based on an integer input.

---
+++ 05_Control_Flow/EnhancedSwitchWithReturn.java
@@ -0,0 +1,33 @@
+/*
+ * Switch Expression with Return
+ *
+ * Demonstrates returning a value
+ * using an enhanced switch expression.
+ */
+
+public class SwitchExpressionReturn {
+
+    public static void main(String[] args) {
+
+        int day = 1;
+
+        String dayOfTheWeek = getDayOfWeek(day);
+
+        System.out.println("day = " + day +
+                ", day Of the Week = " + dayOfTheWeek);
+    }
+
+    public static String getDayOfWeek(int day) {
+
+        return switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+    }
+}
