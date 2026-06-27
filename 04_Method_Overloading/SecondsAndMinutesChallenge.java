4e023832c25209a804b9839ed3c240cc8d5841ff
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 18:15:31 +0530

    Add SecondsAndMinutesChallenge solution
    Implement methods to convert seconds to hours, minutes, and seconds using method overloading.

---
+++ 04_Method_Overloading/SecondsAndMinutesChallenge.java
@@ -0,0 +1,30 @@
+/*
+ * Seconds and Minutes Challenge
+ *
+ * Convert seconds into hours, minutes,
+ * and seconds using overloaded methods.
+ */
+
+public class SecondsAndMinutesChallenge {
+
+    public static String getDurationString(int seconds) {
+
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+
+        return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0 || seconds < 0 || seconds > 59) {
+            return "Invalid Value";
+        }
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+}
