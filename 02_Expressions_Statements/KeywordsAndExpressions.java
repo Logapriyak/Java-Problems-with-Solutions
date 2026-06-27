091d48b97906110fecf7f5ab16702d14a22ee8af
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 13:50:37 +0530

    Add KeywordsAndExpressions example
    Added a Java class demonstrating keywords, expressions, and statements.

---
+++ 02_Expressions_Statements/KeywordsAndExpressions.java
@@ -0,0 +1,22 @@
+/*
+ * Keywords and Expressions
+ *
+ * This example demonstrates:
+ * - Java keywords
+ * - Expressions
+ * - Statements
+ * - Code blocks
+ */
+
+public class KeywordsAndExpressions {
+
+    public static void main(String[] args) {
+
+        int score = 100;
+
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+    }
+}
