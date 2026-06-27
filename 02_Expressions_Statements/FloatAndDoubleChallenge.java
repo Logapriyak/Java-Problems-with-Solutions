748c65ec4538e99859d356d27b8db8f5c27ee468
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 13:52:30 +0530

    Add FloatAndDoubleChallenge solution
    This program converts a weight in pounds to kilograms and prints the result.

---
+++ 02_Expressions_Statements/FloatAndDoubleChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Float and Double Challenge
+ *
+ * Convert pounds to kilograms.
+ */
+
+public class FloatAndDoubleChallenge {
+
+    public static void main(String[] args) {
+
+        double pounds = 200.0;
+        double kilograms = pounds * 0.45359237;
+
+        System.out.println(pounds + " pounds = " + kilograms + " kilograms");
+    }
+}
