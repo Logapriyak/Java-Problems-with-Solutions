acf6b5e4f867414de75d3c2c52f08a17e60fa237
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 13:14:55 +0530

    Add HelloWorld solution
    This program prints 'Hello World' to the console.

---
+++ 01_Basics/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+Problem: Hello World
+
+Description:
+Print "Hello World" to the console.
+
+Topic:
+Java Basics
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
