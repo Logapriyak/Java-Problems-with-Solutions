f6830dc30c2aebbf9189ba6d568735f671110661
Logapriya K <153262180+Logapriyak@users.noreply.github.com>
2026-06-27 13:53:37 +0530

    Add CharBooleanChallenge example
    This class demonstrates the use of Unicode characters and boolean values.

---
+++ 02_Expressions_Statements/CharBooleanChallenge.java
@@ -0,0 +1,18 @@
+/*
+ * Char and Boolean Challenge
+ *
+ * Demonstrates Unicode characters
+ * and boolean values.
+ */
+
+public class CharBooleanChallenge {
+
+    public static void main(String[] args) {
+
+        char unicodeCharacter = '\u00A9';
+        boolean isCustomerOver21 = true;
+
+        System.out.println(unicodeCharacter);
+        System.out.println(isCustomerOver21);
+    }
+}
