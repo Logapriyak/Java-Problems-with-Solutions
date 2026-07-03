e9efbffef46aada932cc4bf836b5ff5d30ecec4b
Logapriya K <klogapriya2004@gmail.com>
2026-07-03 22:15:19 +0530

    Add ParameterlessMethod solution

---
+++ 03_Methods/ParameterlessMethod.java
@@ -0,0 +1,19 @@
+/*
+ * Parameterless Method
+ *
+ * Demonstrates a method with
+ * no parameters and no return value.
+ */
+
+public class ParameterlessMethod {
+
+    public static void main(String[] args) {
+
+        displayMessage();
+    }
+
+    public static void displayMessage() {
+
+        System.out.println("Welcome to Java Programming!");
+    }
+}
