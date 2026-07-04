bcfe1c4e9c08f7d3ef0669f9bf3174692ca75e99
Logapriya K <klogapriya2004@gmail.com>
2026-07-04 13:20:15 +0530

    Add simple for loop example

---
+++ 05_Control_Flow/04_For_Loop/SimpleForLoop.java
@@ -0,0 +1,17 @@
+/*
+ * Simple For Loop
+ *
+ * Demonstrates the use of a basic
+ * for loop to print numbers from
+ * 1 to 5.
+ */
+
+public class SimpleForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Value = " + i);
+        }
+    }
+}
