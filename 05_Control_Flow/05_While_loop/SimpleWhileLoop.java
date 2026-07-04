394e1bff1f474c38292464e6da8b9e8a4e961af6
Logapriya K <klogapriya2004@gmail.com>
2026-07-04 13:31:22 +0530

    Add simple while loop example

---
+++ 05_Control_Flow/05_While_loop/SimpleWhileLoop.java
@@ -0,0 +1,20 @@
+/*
+ * Simple While Loop
+ *
+ * Demonstrates the use of a basic
+ * while loop to print numbers
+ * from 1 to 5.
+ */
+
+public class SimpleWhileLoop {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count <= 5) {
+            System.out.println("Count = " + count);
+            count++;
+        }
+    }
+}
