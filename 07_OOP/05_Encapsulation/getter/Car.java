66f0e3be75a966bb4dd96f11177f6b3d1667c260
Logapriya K <klogapriya2004@gmail.com>
2026-07-15 18:15:22 +0530

    getter example in java Encapsulation

---
+++ 07_OOP/05_Encapsulation/getter/Car.java
@@ -0,0 +1,37 @@
+package getter;
+
+public class Car {
+
+    private String brand = "Tesla";
+    private String model = "X model";
+    private String color = "Red";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public String getBrand(){ // here the variables are non static so then  get method is also non static
+        return brand ;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void displayCar() {
+        System.out.println(
+                doors + "-Doors " + color + " " + brand + " " + model + " " +
+                        (convertible ? "Convertible" : "")
+        );
+    }
+}
---
+++ 07_OOP/05_Encapsulation/getter/Main.java
@@ -0,0 +1,13 @@
+package getter;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.displayCar();
+        System.out.println("Brand: "+ car.getBrand());
+        System.out.println("Color: "+car.getColor());
+        System.out.println("Doors: "+car.getDoors());
+        System.out.println("Model: "+ car.getModel());
+        System.out.println("Is convertible: "+ car.isConvertible());
+    }
+}
